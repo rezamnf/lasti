877581e7cb5459c06b7fd42e3ace9a2074bbb89f
rezamnf <rezamnf05@gmail.com>
2021-11-23 20:42:23 +0700

    ea

---
+++ esp-data.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html><body>
+<?php
+
+$servername = "database";
+
+// REPLACE with your Database name
+$dbname = "MY_DATABASE";
+// REPLACE with Database user
+$username = "root";
+// REPLACE with Database user password
+$password = "MYSQL_ROOT_PASSWORD";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "SELECT id, sensor, temp, reading_time FROM SensorData ORDER BY id DESC";
+
+echo '<table cellspacing="5" cellpadding="5">
+      <tr> 
+        <td>ID</td> 
+        <td>Sensor</td> 
+        <td>Temp</td> 
+        <td>Timestamp</td> 
+      </tr>';
+ 
+if ($result = $conn->query($sql)) {
+    while ($row = $result->fetch_assoc()) {
+        $row_id = $row["id"];
+        $row_sensor = $row["sensor"]; 
+        $row_temp = $row["temp"]; 
+        $row_reading_time = $row["reading_time"];
+        // Uncomment to set timezone to - 1 hour (you can change 1 to any number)
+        //$row_reading_time = date("Y-m-d H:i:s", strtotime("$row_reading_time - 1 hours"));
+      
+        // Uncomment to set timezone to + 4 hours (you can change 4 to any number)
+        //$row_reading_time = date("Y-m-d H:i:s", strtotime("$row_reading_time + 4 hours"));
+      
+        echo '<tr> 
+                <td>' . $row_id . '</td> 
+                <td>' . $row_sensor . '</td> 
+                <td>' . $row_temp . '</td>  
+                <td>' . $row_reading_time . '</td> 
+              </tr>';
+    }
+    $result->free();
+}
+
+$conn->close();
+
+?> 
+</table>
+</body>
+</html>
